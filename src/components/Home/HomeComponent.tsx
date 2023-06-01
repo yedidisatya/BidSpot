b8774b507131a1f2a952ba35f4d7074be4e273a2
Satyanarayana Yedidi <syedidi@teamvelocitymarketing.com>
2023-06-01 14:39:29 +0530

    Test

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import './Home.module.scss';
+import styles from './Home.module.scss';
+
 const Home: React.FC<EmptyObject> = () => {
 
     return (
-        <div className="home">
+        <div className={styles.home}>
             Home
         </div>
     )
