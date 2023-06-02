2dcd9df0f4e13a6c44a874ff002baba790582081
internal\schowdary <sundeep@qualminds.com>
2023-06-02 16:31:04 +0530

    Nav Bar Component has been updated

--- src/components/NavigationBar/NavigationBarComponent.tsx
+++ src/components/NavigationBar/NavigationBarComponent.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import styles from './NavigationBar.module.scss';
+import cn from 'classnames';
 
 const NavigationBar: React.FC<EmptyObject> = () => {
-  return <div className={styles.home}>The Nav Bar</div>;
+  return (
+    <div className={styles.mainNavigation}>
+      <div className="row">
+        <div className="d-flex align-items-center justify-content-center">
+          <div className={cn(styles.navItem, 'mx-5 text-muted')}>
+            <p>EXPLORE BY</p>
+          </div>
+          <div className={cn(styles.navItem, 'mx-5 d-flex')}>
+            <p>Price Range</p>
+            <i className={'fa fa-angle-down'}></i>
+          </div>
+          <div className={cn(styles.navItem, 'mx-5 d-flex')}>
+            <p>Plot By Area</p>
+            <i className={'fa fa-angle-down'}></i>
+          </div>
+          <div className={cn(styles.navItem, 'mx-5 d-flex')}>
+            <p>Sell the Plot</p>
+            <i className={'fa fa-angle-down'}></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { NavigationBar };
