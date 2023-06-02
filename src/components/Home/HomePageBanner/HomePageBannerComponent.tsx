b8f9db9e91d018cd48e3afb2ca94586dd7fb8ead
internal\schowdary <sundeep@qualminds.com>
2023-06-02 19:59:24 +0530

    Sleck slider added

--- src/components/Home/HomePageBanner/HomePageBannerComponent.tsx
+++ src/components/Home/HomePageBanner/HomePageBannerComponent.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
-import styles from './HomePageBanner.module.scss';
+//import styles from './HomePageBanner.module.scss';
+import Slider from 'react-slick';
+// import img1 from '../../../images/img1.jpg';
+// import img2 from '../../../images/img2.jpg';
+// import img3 from '../../../images/img3.jpg';
 
 const HomePageBanner: React.FC<EmptyObject> = () => {
-  return <div className={styles.mainBanner}>THe bAnner</div>;
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <img src="images/img1.jpg" alt="img1111" />
+      </div>
+      <div>
+        <h3>Hello222</h3>
+        <img src="images/img2.jpg" alt="img22221" />
+      </div>
+      <div>
+        <h3>hello33</h3>
+        <img src="images/img3.jpg" alt="img3333" />
+      </div>
+    </Slider>
+  );
 };
 
 export { HomePageBanner };
