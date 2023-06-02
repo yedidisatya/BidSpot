134b806e1a85b9df0267ae822b6f46c6adcc6fb8
internal\schowdary <sundeep@qualminds.com>
2023-06-02 20:39:24 +0530

    Dummy banner data added

--- src/components/Home/HomePageBanner/HomePageBannerComponent.tsx
+++ src/components/Home/HomePageBanner/HomePageBannerComponent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-//import styles from './HomePageBanner.module.scss';
+import styles from './HomePageBanner.module.scss';
 import Slider from 'react-slick';
+import cn from 'classnames';
 // import img1 from '../../../images/img1.jpg';
 // import img2 from '../../../images/img2.jpg';
 // import img3 from '../../../images/img3.jpg';
@@ -16,15 +17,25 @@ const HomePageBanner: React.FC<EmptyObject> = () => {
   return (
     <Slider {...settings}>
       <div>
-        <img src="images/img1.jpg" alt="img1111" />
+        <img
+          className={cn(styles.scrollImg, 'w-100')}
+          src="https://www.w3schools.com/bootstrap5/ny.jpg"
+          alt="img1111"
+        />
       </div>
       <div>
-        <h3>Hello222</h3>
-        <img src="images/img2.jpg" alt="img22221" />
+        <img
+          className={cn(styles.scrollImg, 'w-100')}
+          src="https://www.w3schools.com/bootstrap5/chicago.jpg"
+          alt="img22221"
+        />
       </div>
       <div>
-        <h3>hello33</h3>
-        <img src="images/img3.jpg" alt="img3333" />
+        <img
+          className={cn(styles.scrollImg, 'w-100')}
+          src="https://www.w3schools.com/bootstrap5/la.jpg"
+          alt="img3333"
+        />
       </div>
     </Slider>
   );
