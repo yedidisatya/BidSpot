4a19aa2de5e83a82f496e75c5809a8a2fa464162
internal\schowdary <sundeep@qualminds.com>
2023-06-05 14:09:46 +0530

    Serch by city added

--- src/components/NavigationBar/NavigationBarComponent.tsx
+++ src/components/NavigationBar/NavigationBarComponent.tsx
@@ -24,6 +24,38 @@ const NavigationBar: React.FC<EmptyObject> = () => {
           </div>
         </div>
       </div>
+      <div className={styles.searchByCity}>
+        <div className="row mt-5">
+          <div className={styles.searchByCityTittle}>Explore By Location</div>
+          <div className="d-flex text-center justify-content-center">
+            <div className={cn(styles.cityItem, 'mx-5')}>
+              <img src="https://cdn-icons-png.flaticon.com/128/2465/2465341.png" alt="Bangalore" />
+              <p>Delhi</p>
+            </div>
+
+            <div className={cn(styles.cityItem, 'mx-5')}>
+              <img src="https://cdn-icons-png.flaticon.com/128/1282/1282509.png" alt="Bangalore" />
+              <p>Bangalore</p>
+            </div>
+            <div className={cn(styles.cityItem, 'mx-5')}>
+              <img src="https://cdn-icons-png.flaticon.com/128/3375/3375454.png" alt="Bangalore" />
+              <p>Mumbai</p>
+            </div>
+            <div className={cn(styles.cityItem, 'mx-5')}>
+              <img src="https://cdn-icons-png.flaticon.com/128/510/510020.png" alt="Bangalore" />
+              <p>Hyderabad</p>
+            </div>
+            <div className={cn(styles.cityItem, 'mx-5')}>
+              <img src="https://cdn-icons-png.flaticon.com/128/706/706317.png" alt="Bangalore" />
+              <p>Agra</p>
+            </div>
+            <div className={cn(styles.cityItem, 'mx-5')}>
+              <img src="https://cdn-icons-png.flaticon.com/128/3806/3806632.png" alt="Bangalore" />
+              <p>Vizag</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
