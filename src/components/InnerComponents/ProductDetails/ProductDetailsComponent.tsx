bbb29a6efe7fa150492244c0702cb56eb22698e7
internal\schowdary <sundeep@qualminds.com>
2023-07-03 16:41:58 +0530

    PDP Design updated

--- src/components/InnerComponents/ProductDetails/ProductDetailsComponent.tsx
+++ src/components/InnerComponents/ProductDetails/ProductDetailsComponent.tsx
@@ -88,7 +88,7 @@ const ProductDetails: React.FC<EmptyObject> = () => {
                 <div className={styles.primaryBtn}>Get It Now</div>
               </div>
               <div className="col-md-6">
-                <div className={styles.redBtn}>Check It</div>
+                <div className={styles.redBtn}>Check For Loan</div>
               </div>
             </div>
           </div>
@@ -101,7 +101,141 @@ const ProductDetails: React.FC<EmptyObject> = () => {
           </div>
         </div>
       </div>
-      <div className={cn(styles.section2, 'row my-2')}>Hello</div>
+      <div className={cn(styles.section2, 'row my-2')}>
+        <h3 className={cn(styles.blockHeader, 'text-start')}>Highlights Of Property</h3>
+        <div className={styles.propertyHighlights}>
+          <ul className="text-start p-0">
+            <li>
+              <i className="fa fa-smile-o m-2"></i>Next to Haldiram’s – Green Park
+            </li>
+            <li>
+              <i className="fa fa-smile-o m-2"></i>Near Madhapur Metro Station
+            </li>
+            <li>
+              <i className="fa fa-smile-o m-2"></i>Walkable distance to school
+            </li>
+            <li>
+              <i className="fa fa-smile-o m-2"></i>Has best Air Quality
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={cn(styles.section3, 'row my-2')}>
+        <h3 className={cn(styles.blockHeader, 'text-start')}>Property Details</h3>
+        <div className={styles.propertyDetails}>
+          <div className="row p-3">
+            <h5 className={cn(styles.propertyTitle, 'text-start')}>
+              1,150 SFT Second Floor in Green Park, New Delhi, Delhi
+            </h5>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-3')}>
+                Estimated Market Price
+              </div>
+              <div className={cn(styles.rightData, 'text-start col-md-9')}>
+                <s>9632587</s>
+              </div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-3')}>Bank's Reserve Price</div>
+              <div className={cn(styles.rightData, 'text-start col-md-9')}>7458963</div>
+            </div>
+
+            <hr className="my-3"></hr>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Address</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>
+                Plot No A-10A, 2nd Floor Rear Side, Green Park, New Delhi , Delhi, 110016
+              </div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Property Type</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>Residential Flat</div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Possession</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>Physical Possession</div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Seller</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>ICICI Bank</div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Bulit-Up Area</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>1024 SFT</div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Total Area</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>1,150 SFT</div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Property ID</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>14523</div>
+            </div>
+            <div className="row my-3">
+              <div className="col-md-6">
+                <div className={styles.primaryBtn}>I'm interested</div>
+              </div>
+              <div className="col-md-6">
+                <div className={styles.redBtn}>Check For Loan</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={cn(styles.section4, 'row my-2')}>
+        <h3 className={cn(styles.blockHeader, 'text-start')}>Application Details</h3>
+        <div className={styles.propertyDetails}>
+          <div className="row p-3">
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Application Deadline</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>Open</div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>
+                Earnest Money Deposit
+              </div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>
+                <i className="fa fa-inr"> </i>20,00,000
+              </div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Auction Date & Time</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>Open</div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Private Treaty</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>No</div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Bulit-Up Area</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>1024 SFT</div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>CERSAI </div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>
+                Generate CERSAI Report
+              </div>
+            </div>
+            <div className="row">
+              <div className={cn(styles.leftData, 'text-start col-md-4')}>Property ID</div>
+              <div className={cn(styles.rightData, 'text-start col-md-8')}>14523</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={cn(styles.section5, 'row my-2')}>
+        <h3 className={cn(styles.blockHeader, 'text-start')}>Location In Map</h3>
+        <div className={styles.propertyLocation}>Map Here</div>
+      </div>
+      <div className={cn(styles.section5, 'row my-2')}>
+        <h3 className={cn(styles.blockHeader, 'text-start')}>Similar Property's</h3>
+        <div className={cn(styles.propertyLocation, 'row')}>
+          <div className="col-md-3">Item 1</div>
+          <div className="col-md-3">Item 2</div>
+          <div className="col-md-3">Item 3</div>
+          <div className="col-md-3">Item 4</div>
+        </div>
+      </div>
     </div>
   );
 };
